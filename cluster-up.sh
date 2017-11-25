aa6b8157521b60ae3ed8f4afa97e34d714a404ca
Stefan Prodan <stefan.prodan@gmail.com>
2017-11-25 20:47:09 +0200

    K8S cluster up/down using the latest version

---
+++ cluster-up.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+k8s_version=$(gcloud container get-server-config --format=json | jq -r '.validNodeVersions[0]')
+
+gcloud container clusters create demo \
+    --cluster-version=${k8s_version} \
+    --zone=europe-west3-a \
+    --additional-zones=europe-west3-b,europe-west3-c \
+    --num-nodes=1 \
+    --machine-type=n1-standard-1 \
+    --scopes=default,storage-rw
+
+gcloud container clusters get-credentials demo
+
+kubectl create clusterrolebinding "cluster-admin-$(whoami)" \
+    --clusterrole=cluster-admin \
+    --user="$(gcloud config get-value core/account)"
+
